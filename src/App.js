bb46311446da4535ded160dd5a8d4b2a93dd1961
Rodrigo Pires <rpires27@protonmail.com>
2025-05-03 12:37:47 +0100

    computação em cloud é fixe ou podre?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    setStatus(getRandomStatus());
+  }, []);
+
+  function getRandomStatus() {
+    return Math.random() < 0.5 ? "Fixe" : "Podre";
+  }
+
+  return <div>Computação em Cloud é {status}</div>;
 }
 
 export default App;
